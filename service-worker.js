7aedafdd25d4e23aebc22981e49eddcaed3329db
PavelY88 <ptyur13@gmail.com>
2025-05-26 01:41:26 +0700

    geo_notes_pwa

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,22 @@
-const CACHE_NAME = 'geo-notes-v1';
+const CACHE_NAME = 'geo-notes-v4';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/icon-192.png'
+  '/geo_notes_pwa/',
+  '/geo_notes_pwa/index.html',
+  '/geo_notes_pwa/manifest.json',
+  '/geo_notes_pwa/icon-192.png',
+  '/geo_notes_pwa/icon-512.png',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
 });
