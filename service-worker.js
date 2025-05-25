718b9dfad5d6381418b487462ad3686a232cc1c4
PavelY88 <ptyur13@gmail.com>
2025-05-26 01:31:19 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,22 @@
-const CACHE_NAME = 'geo_notes_v2';
+const CACHE_NAME = 'geo-notes-v4';
 const ASSETS = [
   '/geo_notes_pwa/',
   '/geo_notes_pwa/index.html',
   '/geo_notes_pwa/manifest.json',
   '/geo_notes_pwa/icon-192.png',
-  '/geo_notes_pwa/icon-512.png'
+  '/geo_notes_pwa/icon-512.png',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'
 ];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+});
