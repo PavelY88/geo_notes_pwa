fb2a91568fa6e3121d407b22f7d4b60deb7204d6
PavelY88 <ptyur13@gmail.com>
2025-05-26 22:28:01 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,29 @@
-const CACHE_NAME = 'geo-notes-v5';
+const CACHE_NAME = 'geo-notes-v6';
 const ASSETS = [
-    '/',
-    'index.html',
-    'manifest.json',
-    'icon-192.png',
-    'icon-512.png',
-    'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
-    'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'
+  '/geo_notes_pwa/',
+  '/geo_notes_pwa/index.html',
+  '/geo_notes_pwa/manifest.json',
+  '/geo_notes_pwa/icon-192.png',
+  '/geo_notes_pwa/icon-512.png',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ASSETS))
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Кэширование ресурсов');
+        return cache.addAll(ASSETS);
+      })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
