b91ec2427070c696aa4277cf0f98a6a8e5a4aff9
PavelY88 <ptyur13@gmail.com>
2025-05-26 22:58:47 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'geo-notes-v7';
+const CACHE_NAME = 'geo-notes-v9';
 const ASSETS = [
     './',
     './index.html',
@@ -13,12 +13,19 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(ASSETS))
-    );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(keys => 
+            Promise.all(keys.map(key => 
+                key !== CACHE_NAME ? caches.delete(key) : null
+            ))
     );
 });
